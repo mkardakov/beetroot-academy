ffe7e8a5e30394452f8e7fe2888d8845829e8edd
Michael Kardakov <work.zjiodeu@gmail.com>
2020-05-20 18:51:15 +0300

    Prepare bookstore layout

--- bookstore/environment/bootstrap.sh
+++ bookstore/environment/bootstrap.sh
@@ -4,6 +4,7 @@ apt update
 apt install -y apache2
 apt install -y php
 apt install -y php-xdebug
+apt install -y php-mysql
 apt install -y mysql-server
 cp /var/www/html/environment/xdebug.ini /etc/php/7.2/cli/conf.d/20-xdebug.ini
 systemctl reload apache2
