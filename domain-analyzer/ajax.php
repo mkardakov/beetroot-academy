4a3868e014d26d7448bbfbb8822f3c7a7378c569
Michael Kardakov <work.zjiodeu@gmail.com>
2020-06-16 17:13:17 +0300

    Create template

--- domain-analyzer/ajax.php
+++ domain-analyzer/ajax.php
@@ -2,8 +2,7 @@
 require 'vendor/autoload.php';
 
 $command = new \App\Command\Nslookup();
-echo "<ul>";
-foreach ($command->run($_POST['search']) as $ip) {
-    echo "<li>$ip</li>";
-}
-echo "</ul>";
+$output = $command->run($_POST['search']);
+$whois = new \App\Command\Whois();
+$output = array_merge($output, $whois->run($_POST['search']));
+require "templates/main.phtml";
--- domain-analyzer/index.php
+++ domain-analyzer/index.php
@@ -8,59 +8,8 @@
     <!------ Include the above in your HEAD tag ---------->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <style>
-        body,html{
-            height: 100%;
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            background: #e74c3c !important;
-        }
+    <link rel="stylesheet" href="assets/css/main.css">
 
-        .searchbar{
-            margin-bottom: auto;
-            margin-top: auto;
-            height: 60px;
-            background-color: #353b48;
-            border-radius: 30px;
-            padding: 10px;
-        }
-
-        .search_input{
-            color: white;
-            border: 0;
-            outline: 0;
-            background: none;
-            width: 0;
-            caret-color:transparent;
-            line-height: 40px;
-            transition: width 0.4s linear;
-        }
-
-        .searchbar:hover > .search_input{
-            padding: 0 10px;
-            width: 450px;
-            caret-color:red;
-            transition: width 0.4s linear;
-        }
-
-        .searchbar:hover > .search_icon{
-            background: white;
-            color: #e74c3c;
-        }
-
-        .search_icon{
-            height: 40px;
-            width: 40px;
-            float: right;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border-radius: 50%;
-            color:white;
-            text-decoration:none;
-        }
-    </style>
 </head>
 <!-- Coded with love by Mutiullah Samim-->
 <body>
@@ -69,10 +18,8 @@
         <div class="searchbar">
             <input class="search_input" type="text" name="" placeholder="Search...">
             <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
+            <div id="output"></div>
         </div>
-    </div>
-    <div id="output">
-
     </div>
 </div>
 <script>
